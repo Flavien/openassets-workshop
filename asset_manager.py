91d6e260aced6693f115d596c3404bc013a079ea
Flavien Charlon <flavien@charlon.net>
2015-08-02 11:30:39 +0100

    Setup the main coroutine

--- asset_manager.py
+++ asset_manager.py
@@ -1,3 +1,14 @@
+import asyncio
 
 class AssetManager:
-    pass
+    def __init__(self, bitcoin_core_url):
+        self.bitcoin_core_url = bitcoin_core_url
+
+    def run(self):
+        print("Running the AssetManager...")
+        asyncio.new_event_loop().run_until_complete(self.run_loop())
+        print("AssetManager terminating")
+
+    @asyncio.coroutine
+    def run_loop(self):
+        pass
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from asset_manager import AssetManager
 
 if __name__ == '__main__':
-    asset_manager = AssetManager()
+    bitcoin_core_url = "http://generated_by_armory:G17aGhX5wsGjZeY3vQXRLEXHAq1ont3Wso6yPAHS3HHH@localhost:18332"
+    asset_manager = AssetManager(bitcoin_core_url)
+    asset_manager.run()
