8bab859f8b334fce24ab0d04ce90adf12597087e
Flavien Charlon <flavien@charlon.net>
2015-08-02 12:25:57 +0100

    Add the create_engine function

--- asset_manager.py
+++ asset_manager.py
@@ -1,16 +1,22 @@
 import asyncio
 import bitcoin
+import openassets.protocol
 import providers
 from pprint import pprint
 
 class AssetManager:
     def __init__(self, bitcoin_core_url):
+        self.event_loop = asyncio.new_event_loop()
         self.blockchain = providers.BitcoinCoreProvider(bitcoin_core_url)
         bitcoin.params = bitcoin.TestNetParams
 
+    def create_engine(self):
+        cache = openassets.protocol.OutputCache()
+        return openassets.protocol.ColoringEngine(self.blockchain.get_transaction, cache, self.event_loop)
+
     def run(self):
         print("Running the AssetManager...")
-        asyncio.new_event_loop().run_until_complete(self.run_loop())
+        self.event_loop.run_until_complete(self.run_loop())
         print("AssetManager terminating")
 
     @asyncio.coroutine
