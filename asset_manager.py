bbaa8911aa4058c352564e691836c0da2957b3ec
Flavien Charlon <flavien@charlon.net>
2015-08-02 22:03:21 +0100

    Add all the configuration settings needed

--- asset_manager.py
+++ asset_manager.py
@@ -1,13 +1,19 @@
 import asyncio
 import bitcoin
+import bitcoin.wallet
 import openassets.protocol
-import providers
+import openassets.transactions
 from pprint import pprint
 
 class AssetManager:
-    def __init__(self, bitcoin_core_url):
-        self.event_loop = asyncio.new_event_loop()
-        self.blockchain = providers.BitcoinCoreProvider(bitcoin_core_url)
+    def __init__(self, event_loop, blockchain,
+                 vending_machine_address, storage_address, issuance_address,  satoshis_per_unit):
+        self.event_loop = event_loop
+        self.blockchain = blockchain
+        self.vending_machine_address = vending_machine_address
+        self.storage_address = storage_address
+        self.issuance_address = issuance_address
+        self.satoshis_per_unit = satoshis_per_unit
         bitcoin.params = bitcoin.TestNetParams
 
     def create_engine(self):
@@ -15,12 +21,15 @@ class AssetManager:
         return openassets.protocol.ColoringEngine(self.blockchain.get_transaction, cache, self.event_loop)
 
     def run(self):
-        print("Running the AssetManager...")
+        print("Running the asset manager...")
         self.event_loop.run_until_complete(self.run_loop())
-        print("AssetManager terminating")
+        print("Asset manager terminating")
 
     @asyncio.coroutine
     def run_loop(self):
-        unspent = yield from self.blockchain.list_unspent(addresses=None, min_confirmations=0)
-        pprint(unspent)
-        pass
+        # List unspent transaction outputs
+
+        # Color them using the coloring engine
+        engine = self.create_engine()
+
+        # Process every uncolored output
--- run.py
+++ run.py
@@ -1,11 +1,27 @@
+import asyncio
+import providers
 from asset_manager import AssetManager
 
 #
 # Prerequisites on Windows:
 # - Install OpenSSL: http://slproweb.com/products/Win32OpenSSL.html
 #   e.g. http://slproweb.com/download/Win64OpenSSL_Light-1_0_1p.exe
+#
 
 if __name__ == '__main__':
-    bitcoin_core_url = "http://generated_by_armory:G17aGhX5wsGjZeY3vQXRLEXHAq1ont3Wso6yPAHS3HHH@localhost:18332"
-    asset_manager = AssetManager(bitcoin_core_url)
+    event_loop = asyncio.new_event_loop()
+
+    bitcoin_core_url = "http://<username>:<password>@localhost:18332"
+    chain_base_url = "https://api.chain.com/v1/testnet3/"
+    chain_api = "<apikey>"
+    chain_secret = "<apisecret>"
+
+    vending_machine_address = ""
+    storage_address = ""
+    issuance_address = ""
+
+    bitcoin_core = providers.BitcoinCoreProvider(bitcoin_core_url)
+    # chain = providers.ChainApiProvider(chain_base_url, chain_api, chain_secret, bitcoin_core, event_loop)
+
+    asset_manager = AssetManager(event_loop, bitcoin_core, vending_machine_address, storage_address, issuance_address, 5)
     asset_manager.run()
